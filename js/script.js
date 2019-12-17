9c364c0c8022fa64ffd47de472cfc2efb8f5cc73
Andrea Bottini <andrea.bottini22@gmail.com>
2019-12-17 16:29:18 +0100

    Termine parte Javascript

--- js/script.js
+++ js/script.js
@@ -17,12 +17,27 @@
 // variabileVuota = 'sovrascrivo la variabile di prima';
 //
 // console.log(variabileVuota);
+//
+// var nome = 'che nome hai?'
+// console.log(nome);
+//
+// var somma = 4 + 4 + 4
+// console.log(somma);
+//
+// var divisione = somma / 2
+// console.log(divisione)
 
-var nome = 'che nome hai?'
-console.log(nome);
+var nomeUtente = prompt ('Ciao, posso sapere il tuo nome?');
+console.log(nomeUtente);
+var cognomeUtente = prompt ('Grazie! Ora posso sapere il tuo cognome?');
+console.log(cognomeUtente);
+var colorePreferito = prompt ('Bene! Adesso ho bisogno di conoscere il tuo colore preferito!');
+console.log(colorePreferito);
+var numerObbligatorio = 19;
+console.log(numerObbligatorio);
 
-var somma = 4 + 4 + 4
-console.log(somma);
+var password = nomeUtente + cognomeUtente + colorePreferito + numerObbligatorio;
+console.log(password)
 
-var divisione = somma / 2
-console.log(divisione)
+
+document.getElementById('password').innerHTML = password;
