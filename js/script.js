d0910fd5faee1cc5257b2be54eb779704a720d9c
Andrea Bottini <andrea.bottini22@gmail.com>
2019-12-17 15:31:08 +0100

    prova inserimento variabile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 //alert('Funziona la pagina?')
 
 console.log('provami');
+
+var number = 5;
+
+console.log(number);
+
+var variabileVuota;
+
+console.log(variabileVuota);
+
+variabileVuota = 'inserisco del testo nella variabile che prima era vuota';
+
+console.log(variabileVuota);
